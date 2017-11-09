73594026ef74a7a67d7bbf4e3354dd676725f5de
tyage <namatyage@gmail.com>
2017-11-10 05:59:10 +0900

    fix the fucccking problem

--- src/post.php
+++ src/post.php
@@ -1,12 +1,12 @@
 <?php
 include('config.php');
 
-$filename = strtolower($_POST['filename']);
+$filename = basename(strtolower($_POST['filename']));
 if ($filename == 'is_admin' || preg_match('/\./', $filename)) {
   die('Hello hacker :)');
 }
 
 @mkdir($USER_DIR);
-file_put_contents($USER_DIR . '/' . basename($_POST['filename']), $_POST['contents']);
+file_put_contents($USER_DIR . '/' . $_POST['filename'], $_POST['contents']);
 
 header('Location: /');
