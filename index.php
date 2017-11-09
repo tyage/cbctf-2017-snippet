84cf585bf8c179195509e900f1f23c42ed7dbf20
admin <admin@client-mac2.local>
2017-11-09 23:48:40 +0900

    Update design

--- index.php
+++ index.php
@@ -9,9 +9,22 @@ if (file_exists($USER_DIR . '/is_admin')) {
 <head>
     <meta charset="UTF-8">
     <title>CODE BLUE Snippet</title>
+    <link href="http://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.31.0/codemirror.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.31.0/codemirror.js"></script>
     <style>
+        body {
+            background-image: url(http://ctf.codeblue.jp/images/bg.jpg);
+            background-size: cover;
+            background-position: center center;
+            background-attachment: fixed;
+            font-family: Orbitron;
+        }
+
+        a, body {
+            color: #fff;
+        }
+
         .CodeMirror {
             border: 1px solid #c0c0c0;
         }
@@ -45,28 +58,24 @@ if (file_exists($USER_DIR . '/is_admin')) {
 <body>
     <h1>CODE BLUE Snippet</h1>
 
-    <a href="export.php?dir=<?php echo $USER_DIR ?>">Export</a>&nbsp;|
+    <a href="export.php?dir=<?php echo $USER_DIR ?>">Export</a>
+    <span>&nbsp;|&nbsp;</span>
     <a href="<?php echo $USER_DIR ?>">Your files</a>
+    <hr>
 
-    <hr />
+    <h2>Import</h2>
+    <form action="import.php?dir=<?php echo $USER_DIR ?>" enctype="multipart/form-data" method="POST">
+        <input type="file" name="file">
+        <input type="submit" value="Import">
+    </form>
+    <hr>
 
     <h2>Post</h2>
-    <form action="post.php">
+    <form action="post.php" method="POST">
         <p><input type="text" name="filename" placeholder="filename"></p>
         <p><textarea id="code" name="contents"></textarea></p>
         <div class="btns">
             <input type="submit" value="Create Snippet">
-        </div>
-    </form>
-
-    <hr />
-
-    <h2>Import</h2>
-
-    <form action="import.php?dir=<?php echo $USER_DIR ?>" enctype="multipart/form-data" method="POST">
-        <input type="file" name="file">
-        <div class="btns">
-            <input type="submit" value="Import">
         </div>
     </form>
 
