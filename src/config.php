4702ea02322e715adcc04f990d700a55965d119f
tyage <namatyage@gmail.com>
2017-11-10 00:48:03 +0900

    add flag

--- src/config.php
+++ src/config.php
@@ -1,7 +1,7 @@
 <?php
-$MY_SECRET = 'immortalmageagent';
-$SALT = 'immortalmageagent';
-$FLAG = 'CBCTF{hogehogege}';
+$MY_SECRET = 'a0572802e277a92210eab6aee0332a21';
+$SALT = 'c714025a1500f4f92681d3b952c5b423';
+$FLAG = 'CBCTF{plz fix PHP Bug #72374}';
 
 session_start();
 $USER_DIR = md5($SALT . $_SERVER['REMOTE_ADDR'] . session_id());
